4c63705105f25d20c802e7852e09d21cd28a26ea
Almas Zaurbekov <almaszaurbekov@Almass-MacBook-Air.local>
2024-10-31 18:53:48 +0500

    client prod initial

--- readme.md
+++
@@ -1,7 +0,0 @@
-# Installation
-
-1. `python -m venv venv`
-2. `source venv/bin/activate` (for mac)
-3. `pip install -r requirements.txt`
-
-- You need to add your credentials to the config.json file
--- services/config.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open('config.json', 'r') as file:
-    config = json.load(file)
-
-asst_id = config.get("assistant_id")
-open_api_key = config.get('open_api_key')
-app_secret = config.get("app_secret")
